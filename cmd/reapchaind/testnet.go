c945b52f8fec11bb589e853c7ff577f34c2d0f83
comnic <comnics@gmail.com>
2022-04-18 15:59:52 +0900

    Add parameter.

--- cmd/reapchaind/testnet.go
+++ cmd/reapchaind/testnet.go
@@ -311,6 +311,7 @@ func initTestnetFiles(
 			stakingtypes.NewDescription(nodeDirName, "", "", "", ""),
 			stakingtypes.NewCommissionRates(sdk.OneDec(), sdk.OneDec(), sdk.OneDec()),
 			sdk.OneInt(),
+            "standing",
 		)
 		if err != nil {
 			return err
